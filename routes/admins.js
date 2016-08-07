4f35e9fd2afea3e7d8d4544e797ec96e360ade88
BjarniLeifs <bjarnil10@ru.is>
2016-08-07 17:58:03 +0000

    make and remove for admin, active and moderator

--- routes/admins.js
+++ routes/admins.js
@@ -29,34 +29,131 @@ router.get('/users', (req, res, next) => {
 	});
 	
 });
+
 router.put('/active', (req, res, next) => {
 	"use strict";
-	var check = _.toNumber(req.body.userId);
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET active = ($1) WHERE id = ($2)';
+	let value = [true, req.body.userId];
+	console.log(check);
 
-	if (_.isNumber(check)){
-		return res.status(200).json({message: 'it is a number : '+check});
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now active.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
 	}
 	else {
-		return res.status(400).json({message: 'Provide id of user!'});
+		return res.status(400).json({message: 'Provide id of user to activate.'});
 	}
-
-	return res.status(200).json({message: check});
-});
-router.put('/deactivate/:uid', (req, res, next) => {
-	"use strict";
-});
-router.put('/make/admin/:uid', (req, res, next) => {
-	"use strict";
-});
-router.put('/remove/admin/:uid', (req, res, next) => {
-	"use strict";
 });
 
-router.put('/make/moderator/:uid', (req, res, next) => {
+router.put('/deactivate', (req, res, next) => {
 	"use strict";
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET active = ($1) WHERE id = ($2)';
+	let value = [false, check];
+
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now deactive.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
+	}
+	else {
+		return res.status(400).json({message: 'Provide id of user to deactivate.'});
+	}
 });
-router.put('/remove/moderator/:uid', (req, res, next) => {
+router.put('/make/admin', (req, res, next) => {
 	"use strict";
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET admin = ($1) WHERE id = ($2)';
+	let value = [true, check];
+
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now admin.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
+	}
+	else {
+		return res.status(400).json({message: 'Provide id of user to make admin.'});
+	}
+});
+
+router.put('/remove/admin', (req, res, next) => {
+	"use strict";
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET admin = ($1) WHERE id = ($2)';
+	let value = [false, check];
+
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now no longer admin.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
+	}
+	else {
+		return res.status(400).json({message: 'Provide id of user to remove admin.'});
+	}
+});
+
+router.put('/make/moderator', (req, res, next) => {
+	"use strict";
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET moderator = ($1) WHERE id = ($2)';
+	let value = [true, check];
+
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now moderator.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
+	}
+	else {
+		return res.status(400).json({message: 'Provide id of user to make moderator.'});
+	}
+});
+
+router.put('/remove/moderator', (req, res, next) => {
+	"use strict";
+	let check = _.toNumber(req.body.userId);
+	let table = 'users';
+	let string = 'UPDATE ' +table+ ' SET moderator = ($1) WHERE id = ($2)';
+	let value = [false, check];
+
+	if (_.isNumber(check) && !(_.isNaN(check))) {
+		service.queryStringValue(string, value, (err, result) => {
+			if (result) {
+				return res.status(200).json({message: 'User is now no longer moderator.'});
+			} else {
+				return res.status(400).json({message: 'Error running query to '+ table});
+			} 
+		});
+	}
+	else {
+		return res.status(400).json({message: 'Provide id of user to remove moderator.'});
+	}
 });
 
 
